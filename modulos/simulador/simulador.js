e3384f64cdc6fa46799628f69d4f5fb2f9a46669
JoyceRosana <tavares.rosana@escola.pr.gov.br>
2026-05-26 18:12:28 -0300

    .

--- modulos/simulador/simulador.js
+++ modulos/simulador/simulador.js
@@ -1,11 +1,13 @@
-const canteiros=document.querySelectorAll(".canteiro");
+let x = 0;
+let y = 0;
+const speed = 30;
 
-canteiros.forEach(canteiro=>{
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowRight") x -= speed;
+  if (e.key === "ArrowLeft") x += speed;
+  if (e.key === "ArrowUp") y += speed;
+  if (e.key === "ArrowDown") y -= speed;
 
-canteiro.addEventListener("click",()=>{
-
-canteiro.innerHTML="🥕";
-
-});
-
+  document.getElementById("world").style.transform =
+    `translate(${x}px, ${y}px)`;
 });
