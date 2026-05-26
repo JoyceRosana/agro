c79d52e872b5ab7bdef25d823ece318555c60dc4
JoyceRosana <tavares.rosana@escola.pr.gov.br>
2026-05-26 18:14:42 -0300

    .

--- modulos/simulador/simulador.js
+++ modulos/simulador/simulador.js
@@ -8,6 +8,6 @@ document.addEventListener("keydown", (e) => {
   if (e.key === "ArrowUp") y += speed;
   if (e.key === "ArrowDown") y -= speed;
 
-  document.getElementById("world").style.transform =
+  document.querySelector(".world").style.transform =
     `translate(${x}px, ${y}px)`;
 });
