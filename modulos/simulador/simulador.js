bc9f493460b75ad7b149a0d64bac5b5c0e5b3dcd
JoyceRosana <tavares.rosana@escola.pr.gov.br>
2026-05-27 11:41:59 -0300

    ,

--- modulos/simulador/simulador.js
+++ modulos/simulador/simulador.js
@@ -3,10 +3,10 @@ let y = 0;
 const speed = 30;
 
 document.addEventListener("keydown", (e) => {
-  if (e.key === "ArrowRight") x -= speed;
-  if (e.key === "ArrowLeft") x += speed;
-  if (e.key === "ArrowUp") y += speed;
-  if (e.key === "ArrowDown") y -= speed;
+  if (e.key === "ArrowRight") x += speed;
+  if (e.key === "ArrowLeft") x -= speed;
+  if (e.key === "ArrowUp") y -= speed;
+  if (e.key === "ArrowDown") y += speed;
 
   document.querySelector(".world").style.transform =
     `translate(${x}px, ${y}px)`;
