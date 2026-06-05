90a1499d69fc980f512ab25396132051cd0a77be
JoyceRosana <tavares.rosana@escola.pr.gov.br>
2026-06-04 21:28:45 -0300

    .

--- modulos/simulador/simulador.js
+++ modulos/simulador/simulador.js
@@ -23,23 +23,4 @@ viewport.addEventListener("mouseleave", () => {
 viewport.addEventListener("mouseup", () => {
   isDown = false;
 });
-  // Define a quantidade exata de linhas e colunas
-const linhas = 8;
-const colunas = 15;
-const totalBlocos = linhas * colunas;
-
-// Seleciona o container do HTML
-const grade = document.getElementById('grade');
-
-// Loop para gerar e inserir os blocos automaticamente
-for (let i = 0; i < totalBlocos; i++) {
-  const img = document.createElement('img');
   
-  // CAMINHO ATUALIZADO: Aponta para a sua pasta de assets
-  img.src = '/assets/imagens/simulador/terra.png'; 
-
-  img.className = 'bloco-terra';
-  img.alt = 'Bloco de Terra';
-  
-  grade.appendChild(img);
-}
