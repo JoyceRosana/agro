e3bbc9de74bdc69b83e0eb671d2be0edfcff8955
JoyceRosana <tavares.rosana@escola.pr.gov.br>
2026-06-04 21:13:03 -0300

    .

--- modulos/simulador/simulador.js
+++ modulos/simulador/simulador.js
@@ -22,25 +22,8 @@ viewport.addEventListener("mouseleave", () => {
 
 viewport.addEventListener("mouseup", () => {
   isDown = false;
-});
 
-viewport.addEventListener("mousemove", (e) => {
-
-  if (!isDown) return;
-
-  e.preventDefault();
-
-  const x = e.pageX - viewport.offsetLeft;
-  const y = e.pageY - viewport.offsetTop;
-
-  const walkX = (x - startX);
-  const walkY = (y - startY);
-
-  viewport.scrollLeft = scrollLeft - walkX;
-  viewport.scrollTop = scrollTop - walkY;
-});
-
-// Define a quantidade exata de linhas e colunas
+  // Define a quantidade exata de linhas e colunas
 const linhas = 8;
 const colunas = 15;
 const totalBlocos = linhas * colunas;
@@ -52,12 +35,12 @@ const grade = document.getElementById('grade');
 for (let i = 0; i < totalBlocos; i++) {
   const img = document.createElement('img');
   
-  // ATENÇÃO: Substitua pelo nome ou caminho correto do seu arquivo de imagem
-  img.src = 'terra.png'; 
+  // CAMINHO ATUALIZADO: Aponta para a sua pasta de assets
+  img.src = 'assets/imagens/simulador/terra.png'; 
   
   img.className = 'bloco-terra';
   img.alt = 'Bloco de Terra';
   
   grade.appendChild(img);
 }
-
+});
